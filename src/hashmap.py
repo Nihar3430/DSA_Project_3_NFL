f931883ef3e54d2f7962f9c5f175979d024f6ad3
Nihar Devireddy <145716162+Nihar3430@users.noreply.github.com>
2024-12-02 16:34:44 -0500

    Update hashmap.py

--- src/hashmap.py
+++ src/hashmap.py
@@ -1,3 +1,5 @@
+import time
+
 class HashMap:
     class Pair:
         def __init__(self, gameID_new, offense, defense, first_down_new, yards_new, rush_attempts_new, passes_new, incomplete_new, touchdown_new, sack_new, interception_new, fumble_new):
@@ -21,35 +23,34 @@ class HashMap:
     def get_hash(self, key):
         hash_num = 0
         for i, j in enumerate(key):
-            hash_num += ord(j) * (31 ** i) # sum of powers of 31 of ASCII values
+            hash_num += int(ord(j) * (31 ** i)) # sum of powers of 31 of ASCII values
         return hash_num % self.size
 
     def insert(self, gameID, offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble):
         new_node = self.Pair(gameID, offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble)
         list_key = self.get_hash(gameID)
-        list_value = [gameID, [offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble]]
-
+        list_value = [gameID, [offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble, gameID]]
         bucket = self.hashmap[list_key]
-        for i, j in enumerate(bucket):
-            k, v = j
-            if k == gameID:
-                bucket[i][2] += first_down
-                bucket[i][3] += yards
-                bucket[i][4] += rush_attempts
-                bucket[i][5] += passes
-                bucket[i][6] += incomplete
-                bucket[i][7] += touchdown
-                bucket[i][8] += sack
-                bucket[i][9] += interception
-                bucket[i][10] += fumble
+
+        for i in bucket:
+            if i[0] == gameID:
+                bucket[1][2] += first_down
+                bucket[1][3] += yards
+                bucket[1][4] += rush_attempts
+                bucket[1][5] += passes
+                bucket[1][6] += incomplete
+                bucket[1][7] += touchdown
+                bucket[1][8] += sack
+                bucket[1][9] += interception
+                bucket[1][10] += fumble
                 return
         bucket.append(list_value)
 
     def search(self, gameID):
         list_key = self.get_hash(gameID)
         bucket = self.hashmap[list_key]
-        for k, v in bucket:
-            if k == gameID:
-                return v
+        for i in bucket:
+            if i[0] == gameID:
+                return i[1]
         return None
 

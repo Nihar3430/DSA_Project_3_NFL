2ea26b1003c1a39d84050a40b8d3d769e38d32e8
Nihar Devireddy <145716162+Nihar3430@users.noreply.github.com>
2024-12-04 20:14:41 -0500

    Final hashmap.py

--- src/hashmap.py
+++ src/hashmap.py
@@ -1,35 +1,22 @@
+#learned implementation from https://www.geeksforgeeks.org/hash-map-in-python/
 import time
 
 class HashMap:
-    class Pair:
-        def __init__(self, gameID_new, offense, defense, first_down_new, yards_new, rush_attempts_new, passes_new, incomplete_new, touchdown_new, sack_new, interception_new, fumble_new):
-            self.gameID = gameID_new
-            self.offense = offense
-            self.defense = defense
-            self.first_down = first_down_new
-            self.yards = yards_new
-            self.rush_attempts = rush_attempts_new
-            self.passes = passes_new
-            self.incomplete = incomplete_new
-            self.touchdown = touchdown_new
-            self.sack = sack_new
-            self.interception = interception_new
-            self.fumble = fumble_new
-
+    #create hashmap
     def __init__(self):
-        self.size = 500
+        self.size = 1000
         self.hashmap = [[] for _ in range(self.size)]
 
+    #get hash of gameID using hashing function with powers of 31 of ASCII
     def get_hash(self, key):
         hash_num = 0
         key = str(key)
-        #print(f"Debug: key = {key}, type = {type(key)}")  # Debug statement
         for i, j in enumerate(key):
             hash_num += int(ord(j) * (31 ** i))
         return hash_num % self.size
 
+    #insert into hashmap
     def insert(self, gameID, offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble):
-        new_node = self.Pair(gameID, offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble)
         list_key = self.get_hash(gameID)
         #list_value = [gameID, [offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble, gameID]]
         #bucket = self.hashmap[list_key]
@@ -48,6 +35,7 @@ class HashMap:
                 return
         self.hashmap[list_key].append([gameID, [offense, defense, first_down, yards, rush_attempts, passes, incomplete, touchdown, sack, interception, fumble, gameID]])
 
+    #search hashmap with gameID as key
     def search(self, gameID):
         list_key = self.get_hash(gameID)
         #bucket = self.hashmap[list_key]
